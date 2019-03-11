cbc7fdd8c5b680e82ae7ef30e66904707b5ccbdb
afzal.shaikh <afzal.shaikh@sky.uk>
2019-03-11 17:57:09 +0000

    Adding pytest session start/end functions

--- conftest.py
+++ conftest.py
@@ -2,3 +2,14 @@ import sys, os
 
 print("""Adding "src" to Current path """.format(os.path.join(os.path.dirname(__file__))))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+
+def pytest_sessionstart(session):
+    """ before session.main() is called. """
+    pass
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """ whole test run finishes. """
+    pass
+
